c97fa5cb81acc1b22c89053fdbbdf06398f7688c
aalinc9 <chingheng113@gmail.com>
2018-11-27 09:33:05 -0500

    back up

--- utils/k_distance_plot.py
+++ utils/k_distance_plot.py
@@ -2,16 +2,14 @@ from sklearn.neighbors import NearestNeighbors
 import matplotlib.pyplot as plt
 from utils import data_utils
 
-id_df, bi_df, mrs_df, nih_df = data_utils.get_tsr()
-mrs_target = 3
-bi_mrs = bi_df[mrs_df['discharged_mrs'] == mrs_target]
+id_df, bi_df, mrs_df, nih_df = data_utils.get_tsr(5, 'is')
 # a minimum minPts can be derived from the number of dimensions D in the data set, as minPts ≥ D + 1
-minPts = bi_mrs.shape[1]+1
+minPts = bi_df.shape[1]+1
 # The value for ε can then be chosen by using a k-distance graph, plotting the distance to the k = minPts-1
 k = minPts-1
-nbrs = NearestNeighbors(n_neighbors=k).fit(bi_mrs)
-distances, indices = nbrs.kneighbors(bi_mrs)
+nbrs = NearestNeighbors(n_neighbors=k).fit(bi_df)
+distances, indices = nbrs.kneighbors(bi_df)
 distanceDec = sorted(distances[:,k-1], reverse=True)
-plt.plot(list(range(1, bi_mrs.shape[0]+1)), distanceDec)
+plt.plot(list(range(1, bi_df.shape[0]+1)), distanceDec)
 plt.show()
 
--- visualization/pca.py
+++ visualization/pca.py
@@ -3,28 +3,33 @@ import matplotlib.pyplot as plt
 from sklearn import decomposition
 from utils import data_utils
 from sklearn.preprocessing import scale
+from pandas.core.frame import DataFrame
 
 
 def do_pca(data, labels):
-    pca = decomposition.PCA(n_components=10)
+    pca = decomposition.PCA(n_components=2)
     data = scale(data)
     pca.fit(data)
+    '''
+    # Variance explains
     va = pca.explained_variance_ratio_
     print(va)
-    #Cumulative Variance explains
-    va1=np.cumsum(np.round(pca.explained_variance_ratio_, decimals=4)*100)
+    # Cumulative Variance explains
+    va1 = np.cumsum(np.round(pca.explained_variance_ratio_, decimals=4)*100)
     print(va1)
-    # plt.plot(va1)
-
+    plt.plot(va1)
+    '''
     transformed_data = pca.transform(data)
+    data_utils.save_dataframe_to_csv(DataFrame(transformed_data), 'pca')
+
     plt.scatter(transformed_data[:, 0], transformed_data[:, 1], c=labels.values.ravel(),
                 cmap=plt.cm.nipy_spectral, edgecolor='k', alpha=0.5)
     plt.xlabel('component 1')
-    plt.ylabel('ylabelcomponent 2')
+    plt.ylabel('component 2')
     plt.show()
 
 
 if __name__ == '__main__':
-    id_df, bi_df, mrs_df, nih_df = data_utils.get_tsr(4, 'is')
+    id_df, bi_df, mrs_df, nih_df = data_utils.get_tsr(5, 'is')
     # bi_df = data_utils.scale(bi_df)
     do_pca(bi_df, mrs_df)
